3c81834ccff7cd7891c73f7e980408bcd7430131
snehhagarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-02-18 00:12:27 +0530

    Add files via upload

---
+++ sumofarray.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    {
+       int n,i,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+        for(i=0;i<n;i++)
+        {
+              c=c+a[i];
+        }printf("%d",c);
+
+
+    return 0;
+}
+
+
+    return 0;
+}
+
