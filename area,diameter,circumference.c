030e41dc5f9203053837c98b01184425545a435e
snehhagarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-02-19 00:22:01 +0530

    Add files via upload

---
+++ area,diameter,circumference.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   int r=7,d,c,a;
+   d=2*r;
+   c=2*3.14*r;
+   a=r*r*3.14;
+   printf("the diameter of the circle is: %d\n",d);
+   printf("the circumference of the circle is: %d\n",c);
+   printf("the area of the circle is: %d\n",d);
+
+
+
+    return 0;
+}
+
+
+
+
+
