f1a0074e9efb7eb284d34634b811711482418ed9
snehhagarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-02-19 00:26:55 +0530

    Add files via upload

---
+++ operators-on-two-variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a=12,b=6,c,d,e,f;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    printf("the sum of the numbers is: %d\n",c);
+    printf("the subtraction of the numbers is: %d\n",d);
+    printf("the multiply of the numbers is: %d\n",e);
+    printf("the division of the numbers is: %d\n",f);
+
+
+    return 0;
+}
+
+
+
