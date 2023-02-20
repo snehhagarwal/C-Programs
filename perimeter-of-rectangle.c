d80ad140c0218cc986f98769d87dc4d9777ff850
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-02-20 20:42:34 +0530

    Add files via upload

---
+++ perimeter-of-rectangle.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int length=4,breadth=6,perimeter;
+    perimeter=2*(length+breadth);
+    printf("the perimeter of the rectangle is: %d",perimeter);
+
+    return 0;
+}
+
+
+
