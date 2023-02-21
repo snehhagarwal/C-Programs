6ae2579a831075d9ded7fdfd5dd2a2636b04f312
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-02-21 23:19:23 +0530

    Add files via upload

---
+++ celsius-to-fahrenheit.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int c,f;
+    scanf("%d",&c);
+    f=(c*1.8)+32;
+    printf("fahrenheit is: %d",f);
+
+    return 0;
+}
+
+
---
+++ length-in-metre-km.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int l, c,k;
+    scanf("%d",&l);
+    c=l/100;
+    k=l/100000;
+    printf("the length in metre is: %d\n ",c);
+    printf("the length in kilometre is: %d\n ",k);
+
+    
+    return 0;
+
+}
+
+
+
