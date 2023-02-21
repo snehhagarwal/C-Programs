03326e2298a9f3b8a3222e2b40e2dddbd2d4bf64
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-02-22 00:07:56 +0530

    Add files via upload

---
+++ raising-a-number.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int x,y,z;
+    scanf("%d%d",&x,&y);
+    z=pow(x,y);
+    printf("power %d",z);
+
+    return 0;
+}
+
