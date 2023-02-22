52a5b36d38ef3530ca1991fd1f0706dc7501797a
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-02-23 00:16:28 +0530

    Add files via upload

---
+++ area-of-equilateral-triangle.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a=4;
+    float t;
+    t=(sqrt(3)/4)*pow(a,2);
+    printf("the area is: %f",t);
+
+    return 0;
+}
+
