2c5a9141cf4d7b25c8435e3de81a4ca6f57b8387
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-02-23 00:15:22 +0530

    Add files via upload

---
+++ third-angle-of-triangle.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int x,y,z;
+    scanf("%d",&x);
+    scanf("%d",&y);
+    z=180-(x+y);
+    printf("the third angle is %d",z);
+
+    return 0;
+}
+
