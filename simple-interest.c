7f1b4593f52d928d3e451d22221c128f72171d68
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-02-24 01:16:32 +0530

    Add files via upload

---
+++ simple-interest.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int p,r,t;
+    scanf("%d %d %d",&p,&r,&t);
+    float si;
+    si=(p*r*t)/100;
+    printf("the simple interest is: %f",si);
+
+    return 0;
+}
+
