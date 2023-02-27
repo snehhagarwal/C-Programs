eb8441687fc6112dc11e55576d3541f64ca4e4f2
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-02-28 01:22:41 +0530

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    
+    int n,i,e=0,o=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+        for(i=0;i<n;i++)
+        {
+            if(a[i]%2==0)
+             e+=1;
+            else
+             o+=1;
+            
+             
+        }printf("%d %d",e,o);
+
+
+    return 0;
+
+
+    return 0;
+}
+
