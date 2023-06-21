0daa8d906d3c7aefe09de1e792d4d6be6087de59
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-06-21 23:04:06 +0530

    Add files via upload

---
+++ printing table of a number till limit.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int n,l;
+    scanf("%d%d",&n,&l);
+    for(int i=1;i<=l;i++)
+    {
+        printf("%d * %d = %d\n",n,i,n*i);
+    }
+
+    return 0;
+}
+
