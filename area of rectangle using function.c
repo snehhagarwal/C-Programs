e02a266bbb69f25e037eff674313d319efb5c82e
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-06-23 23:03:31 +0530

    Add files via upload

---
+++ area of rectangle using function.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+float area(float h,float w)
+{   float area_rect;
+    area_rect=h*w;
+    return area_rect;
+}
+
+int main()
+{
+    float h,w,area_ofrectangle;
+    scanf("%f%f",&h,&w);
+    area_ofrectangle=area(h,w);
+    printf("%.2f",area_ofrectangle);
+
+    return 0;
+}
+
