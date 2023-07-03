941a2539fcb01160580ead5bb79ac6efad2387d4
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-07-04 00:52:31 +0530

    Add files via upload

---
+++ unique element using array.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#define size 10
+int main()
+{
+    int arr[size],c=0,uniquecount=0;
+    for (int i=0;i<size;i++)
+    {
+        printf("enter the element:");
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            if (i==j)
+               continue;
+            if (arr[i]==arr[j])
+               c+=1;
+        }if (c!=1) 
+        {
+            printf("the unique element is %d\n",arr[i]);
+            uniquecount++;
+        }c=0;
+    }printf("the number of unique elements is : %d",uniquecount);
+
+    return 0;
+}
+
