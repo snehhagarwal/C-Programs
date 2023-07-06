882f572946528e441b438f7c9224d10b4d48d5f1
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-07-06 23:11:05 +0530

    Add files via upload

---
+++ rotate array by n positions.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#define size 4
+int main()
+{
+    int arr[size],n,temp;
+    for(int i=0;i<size;i++)
+    {
+        printf("enter the element:");
+        scanf("%d",&arr[i]);
+    }printf("enter the number by which array is to be rotated:");
+    scanf("%d",&n);
+    while(n!=0)
+    {  temp=arr[0];
+       for(int i=0;i<size;i++)
+       {
+           arr[i]=arr[i+1];
+       }arr[size-1]=temp;
+       n--;
+    }for(int i=0;i<size;i++)
+    {
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
+
