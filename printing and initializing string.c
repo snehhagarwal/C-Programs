0039c0c3258c459b32e275e9ed1a0c4dd3415ab1
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-07-07 21:33:52 +0530

    Add files via upload

---
+++ printing and initializing string.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char str[14]="Sneha Agarwal";
+    for(int i=0;str[i]!='\0';i++)
+    {
+        printf("%c",str[i]);
+    }
+    
+
+    return 0;
+}
+
