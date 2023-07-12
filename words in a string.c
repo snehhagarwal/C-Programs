4f60832065844d1f6751d39efbe23c15178bc0e2
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-07-12 23:21:39 +0530

    Add files via upload

---
+++ words in a string.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    char str[100],c=0;
+    gets(str);
+    for(int i=0;str[i]!='\0';i++)
+    {
+        if  (str[i]==' ')
+             c++;
+    }
+    printf("the total number of words are %d",c);
+    return 0;
+}
+
