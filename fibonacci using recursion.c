077ee982d9685627f4de0a6e645fbfb4c792ea0a
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-07-15 23:41:19 +0530

    Add files via upload

---
+++ fibonacci using recursion.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+int fibonacci(int n)
+{
+    if (n==0)
+       return 0;
+    if (n==1)
+       return 1;
+    return fibonacci(n-1)+fibonacci(n-2);
+}
+int main()
+{   int n;
+    printf("enter the number ");
+    scanf("%d",&n);
+    int result=fibonacci(n);
+    printf("%d",result);
+    return 0;
+}
+
