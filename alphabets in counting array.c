7cb1c8bc11b5cb86b2ceb0f5363b70bc6e11b4d5
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-07-20 22:07:53 +0530

    Add files via upload

---
+++ alphabets in counting array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define size 8
+int main()
+{
+    char arr[size]={'k','i','b','r','c','k','z','m'};
+    int countarr[26];
+    for (int i=0;i<26;i++)
+    {
+        countarr[arr[i]-97]++;
+    }
+    for (int i=0;i<26;i++)
+    {
+        printf("the letter %c appeared %d times\n",i+97,countarr[i]);
+    }
+    return 0;
+}
+
